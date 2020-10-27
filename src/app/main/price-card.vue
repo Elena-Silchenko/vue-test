d03c48fad603afa4b650f538433b591737f0fd23
Elen Silchenko <elen.silchenko@gmail.com>
2020-10-27 19:05:08 +0100

    pricings

---
+++ src/app/main/price-card.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-card class="mx-auto" max-width="280" elevation="10" outlined tile>
+    
+    <v-card-title class="d-flex justify-space-between">
+      <div>
+        {{ name }}
+      </div>
+
+      <div style="color: #6200ea;">
+        &euro; {{ cost | round }},<sup>{{ cost | modulo }}</sup>
+      </div>
+    </v-card-title>
+
+    <v-card-text>
+      <v-list class="transparent">
+        <v-list-item v-for="feature in Features" :key="feature.id">
+          <v-list-item-icon>
+            <v-icon v-if="features.indexOf(feature.id) >= 0">mdi-checkbox-marked-outline</v-icon>
+            <v-icon v-else>mdi-checkbox-blank-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ feature.title }}</v-list-item-title>
+          <v-list-item-subtitle class="text-right">
+            {{ feature.description }}
+          </v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+
+    <v-divider class="mx-4" />
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2"
+        text
+        @click="order()">
+        Order Now
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  const Features = [
+    {
+      id: "f1",
+      title: "Feature 1",
+      description: "Some description of feature 1",
+    },
+    {
+      id: "f2",
+      title: "Feature 2",
+      description: "Some description of feature 2",
+    },
+    {
+      id: "f3",
+      title: "Feature 3",
+      description: "Some description of feature 3",
+    },
+    {
+      id: "f4",
+      title: "Feature 4",
+      description: "Some description of feature 4",
+    },
+  ]
+
+  export default {
+    props: {
+      id: String,
+      name: String,
+      cost: Number,
+      features: Array,
+    },
+
+    data: () => ({
+      Features: Features,
+    }),
+
+    filters: {
+      round(value) {
+        return Math.floor(value)
+      },
+
+      modulo(value) {
+        return Math.round(100 * (value - Math.floor(value)))
+      }
+    },
+
+    methods: {
+      order() {
+        alert(this.id)
+      }
+    }
+}
+</script>
--- src/app/main/price.vue
+++ src/app/main/price.vue
@@ -1,132 +1,44 @@
 <template>
   <v-row style="padding: 3em 0;">
-    <v-card class="mx-auto" max-width="400" elevation="10" outlined tile>
-
-      <v-card-title>Basic</v-card-title>
-
-      <v-card-subtitle>&euro; 4,95</v-card-subtitle>
-
-      <v-card-text>
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-checkbox-marked-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Feature 1</v-list-item-title>
-            <v-list-item-subtitle class="text-right">
-              description
-            </v-list-item-subtitle>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-checkbox-blank-outline</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-title>Feature 2</v-list-item-title>
-
-            <v-list-item-subtitle class="text-right">
-              description
-            </v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-
-      <v-divider class="mx-4" />
-
-      <v-card-actions>
-        <v-btn
-          color="deep-purple lighten-2"
-          text
-          @click="reserve">
-          Order Now
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card class="mx-auto" max-width="400" elevation="10" outlined tile>
-
-      <v-card-title>Advanced</v-card-title>
-
-      <v-card-subtitle>&euro; 19,95</v-card-subtitle>
-
-      <v-card-text>
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-checkbox-marked-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Feature 1</v-list-item-title>
-            <v-list-item-subtitle class="text-right">
-              description
-            </v-list-item-subtitle>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-checkbox-blank-outline</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-title>Feature 2</v-list-item-title>
-
-            <v-list-item-subtitle class="text-right">
-              description
-            </v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-
-      <v-divider class="mx-4" />
-
-      <v-card-actions>
-        <v-btn
-          color="deep-purple lighten-2"
-          text
-          @click="reserve">
-          Order Now
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card class="mx-auto" max-width="400" elevation="10" outlined tile>
-
-      <v-card-title>Premium</v-card-title>
-
-      <v-card-subtitle>&euro; 49,95</v-card-subtitle>
-
-      <v-card-text>
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-checkbox-marked-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Feature 1</v-list-item-title>
-            <v-list-item-subtitle class="text-right">
-              description
-            </v-list-item-subtitle>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-checkbox-blank-outline</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-title>Feature 2</v-list-item-title>
-
-            <v-list-item-subtitle class="text-right">
-              description
-            </v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-
-      <v-divider class="mx-4" />
-
-      <v-card-actions>
-        <v-btn
-          color="deep-purple lighten-2"
-          text
-          @click="reserve">
-          Order Now
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+    <price-card v-for="card in priceList"
+      :key="card.id"
+      :id="card.id"
+      :name="card.name"
+      :cost="card.cost"
+      :features="card.features"
+    />
   </v-row>
 </template>
+
+<script>
+  import PriceCard from "./price-card"
+
+  export default {
+    data: () => ({
+      priceList: [
+        {
+          id: "basic",
+          name: "Basic",
+          cost: 9.95,
+          features: ["f1", "f2"],
+        },
+        {
+          id: "advanced",
+          name: "Advanced",
+          cost: 14.95,
+          features: ["f2", "f3"],
+        },
+        {
+          id: "premium",
+          name: "Premium",
+          cost: 49.95,
+          features: ["f1", "f2", "f3", "f4"],
+        },
+      ]
+    }),
+
+    components: {
+      "price-card": PriceCard,
+    }
+  }
+</script>
